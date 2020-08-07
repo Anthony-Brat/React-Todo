3699bb940734b925c26c19fe09e9030dc5969b4a
Anthony Bratcher <tonybrat74@gmail.com>
2020-08-07 14:48:44 -0700

    initial

--- src/App.js
+++ src/App.js
@@ -1,4 +1,35 @@
 import React from 'react';
+import ReactDOM from "react-dom";
+import TodoList from "./components/TodoList";
+import TodoForm from "./components/TodoForm";
+
+const todos = [
+  {
+    task: "Research Intermitten Fasting",
+    id: 3,
+    completed: false,
+  },
+  {
+    task: "Spray Yard with Tick Killer",
+    id: 1,
+    completed: false,
+  },
+  {
+    task: "Give Dogs Bath",
+    id: 2,
+    completed: false,
+  },
+  {
+    task: "Watch Heavy Bag Workout",
+    id: 4,
+    completed: false,
+  },
+  {
+    task: "Wash Car",
+    id: 5,
+    completed: false,
+  },
+];
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,21 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from "react";
+import Todo from "./Todo"
+
+const TodoList = props => {
+
+
+    return(
+        <div className = "todo-list">
+            {props.todos.map(todo => (
+                <Todo/>
+
+            ))}
+            <button className= "clear-button">
+                Clear Completed
+            </button>
+        </div>
+    );
+}
+export default TodoList;
